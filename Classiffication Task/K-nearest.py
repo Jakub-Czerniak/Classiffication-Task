98ebab58c0905467a31b32104bdcf010dec5781c
Kuba <Kuba@DESKTOP-FGR0SFK>
2023-03-22 10:25:56 +0100

    Added validation data and confusion matrix.

--- Classiffication Task/K-nearest.py
+++ Classiffication Task/K-nearest.py
@@ -28,32 +28,50 @@ data["democrat, republican"].hist(bins=3)
 X = data.drop("democrat, republican", axis=1)
 y = data["democrat, republican"]
 
-
 for column in X:
     X[column] = X[column].map({'y':1, '?':0 , 'n':-1})
 
-training_data = X.sample(frac=0.8, random_state=3)
+r_state=3
+training_data = X.sample(frac=0.6, random_state=r_state)
 testing_data = X.drop(training_data.index)
-y_training = y.sample(frac=0.8,random_state=3)
+validation_data = testing_data.sample(frac=0.5,random_state=r_state)
+testing_data = testing_data.drop(validation_data.index)
+
+y_training = y.sample(frac=0.6,random_state=r_state)
 y_testing = y.drop(y_training.index)
+y_validation = y_testing.sample(frac=0.5,random_state=r_state)
+y_testing = y_testing.drop(y_validation.index)
+
 y_training = y_training.values
 y_testing = y_testing.values
 y = y.values
 
+#print(str(len(testing_data.index)))
+#print(str(len(training_data.index)))
+#print(str(len(validation_data.index)))
+
 success=0
 count=0
-for index, row in testing_data.iterrows():
+democrat_tp=0
+democrat_fp=0
+democrat_tn=0
+democrat_fn=0
+for index, row in validation_data.iterrows():
     distances = np.linalg.norm(training_data - row, axis=1)
     k = 5
     nearest_neighbours_ids = distances.argsort()[:k]
-    #print(index)
-    #print(nearest_neighbours_ids)
-    #print(distances)
     nearest_neighbours_party = y_training[nearest_neighbours_ids]
-    #print(nearest_neighbours_party)
     prediction = scipy.stats.mode(nearest_neighbours_party, keepdims=False)
     if(prediction.mode == y[index]):
         success+=1
+        if(y[index]=='democrat'):
+            democrat_tp+=1
+        elif(y[index]=='republican'):
+            democrat_tn+=1
+    elif(prediction.mode=='democrat'):
+        democrat_fp+=1
+    else:
+        democrat_fn+=1
     print('Row: ' + str(index) + ' Party: ' + y[index] + ' Prediction: ' + prediction.mode  )
     count+=1
 
@@ -61,3 +79,20 @@ success_rate=success/count
 print('Success count: ' + str(success))
 print('Overall count: ' + str(count))
 print('Success rate: ' + str(int(success_rate*100)) + '%')
+
+print('Democrat')
+print('True positives: ' + str(democrat_tp))
+print('False positives: ' + str(democrat_fp))
+print('True negatives: ' + str(democrat_tn))
+print('False negatives: ' + str(democrat_fn))
+
+republican_tp=democrat_tn
+republican_fp=democrat_fn
+republican_tn=democrat_tp
+republican_fn=democrat_fp
+
+print('Republican')
+print('True positives: ' + str(republican_tp))
+print('False positives: ' + str(republican_fp))
+print('True negatives: ' + str(republican_tn))
+print('False negatives: ' + str(republican_fn))
